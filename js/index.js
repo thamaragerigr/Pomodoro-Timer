b47368bde26fd81c4850366b876f6a1687b03016
Thamara Gerig <thamaragerigr@gmail.com>
2020-01-09 13:26:08 +0100

    Aspectos esteticos
    Layout solucionado, falta cambiar fuente, arreglos menores de input, corregir reloj

--- js/index.js
+++ js/index.js
@@ -1,30 +1,52 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton){
-       this.durationInput = durationInput;
-       this.startButton = startButton;
-       this.pauseButton = pauseButton;
 
-       this.startButton.addEventListener('click', this.start);
-       this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => {
-      this.tick();
-      this.interval = setInterval(this.tick, 1000);
-    };
-
-    pause = () => {
-      clearInterval(this.interval);
-    };
-
-    tick = () => {
-        const timeRemaining = parseFloat(this.durationInput.value);
-        this.durationInput.value = timeRemaining -1 ;
-    };
-}
-
+//TIMER
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector("circle");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+
+let currentOffset= 0;
+let duration;
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration){
+      duration = totalDuration;
+    },
+
+    onTick(timeRemaining){
+      circle.setAttribute('stroke-dashoffset', 
+        perimeter * timeRemaining / duration - perimeter
+      )
+    },
+
+    onComplete(){
+      console.log('pausa');
+    }
+});
+
+
+
+
+
+//MODAL BOX
+const modal = document.getElementById("modal");
+const info = document.getElementById("info");
+const span = document.getElementsByClassName("close")[0];
+
+info.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
